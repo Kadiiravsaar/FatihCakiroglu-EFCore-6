2fc2acd6ee664c197cbf770f3790a9f08af3d148
kadiiravsaar <kadiiravsar@gmail.com>
2023-02-16 15:51:39 +0300

    AppDbContext added. products.cs added

---
+++ EFCore.DatabaseFirst/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.DatabaseFirst.DataAccessLayer
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public DbSet<Product> Products { get; set; }
+        // DbSet<Product> -Burada yazdığın isim veritabanındaki isme denk gelmektedir-
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-G34LNC7;Initial Catalog=EFCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+           
+           // farklı farklı db'ye bağlanabilmek için bi builder alıyoruz.
+        }
+
+        // override yapıp sql server'e bağlantı cümleciği yazmak
+    }
+}
---
+++ EFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using EFCore.DatabaseFirst.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name} {p.Price}");
+    });
+   
+
+    // neden using kullandık? : 
+    // Cevap: İşim bittiği zaman Bu DbContext memory'den dispose(elden çıkartmak, ortadan kaldırmak anlamına gelir.) olsun.
+    // using bittiği anda DbContext iptal olur.
+}
