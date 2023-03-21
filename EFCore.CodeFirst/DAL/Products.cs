d511b9310d3faf7c4418b023a7f7fd98d3594c20
kadiiravsaar <kadiiravsar@gmail.com>
2023-03-21 14:51:59 +0300

    DbContext States

--- EFCore.CodeFirst/DAL/Products.cs
+++ EFCore.CodeFirst/DAL/Products.cs
@@ -10,9 +10,9 @@ namespace EFCore.CodeFirst.DAL
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; } 
+        public decimal Price { get; set; } 
         public int Stock { get; set; }
-        public string Barcode { get; set; } 
+        public int Barcode { get; set; } 
 
       
     
--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -6,15 +6,33 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var products = await _context.products.ToListAsync();
+    #region State Örnek
 
-    products.ForEach(p=>
+    //var newProduct = new Products { Name = "Kalem 200", Price = 500, Stock = 25, Barcode = 1520 };
+
+    //Console.WriteLine($"İlk State {_context.Entry(newProduct).State}"); GenelNotlar.json 29
+
+    //await _context.products.AddAsync(newProduct);
+
+    //Console.WriteLine($"Son State {_context.Entry(newProduct).State}");
+
+
+
+    //await _context.SaveChangesAsync();
+    //Console.WriteLine($" SaveChanges'den State {_context.Entry(newProduct).State}");
+
+    #endregion
+
+    var products = await _context.products.AsNoTracking().ToListAsync();
+
+    products.ForEach(p =>
     {
-        Console.WriteLine($"{p.Id}:{p.Name}");
+
+        p.Stock += 10;
+        //Console.WriteLine($"{p.Id}:{p.Name}:{p.Price} state : {state}");
 
     });
 
-    
-
+    await _context.SaveChangesAsync();
 
 }
